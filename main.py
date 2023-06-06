05b27c4edaec6056c583f2245100d57b0a88d305
Research Development Team <112861917+Research-Development5@users.noreply.github.com>
2023-06-06 11:34:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import numpy as np
 import streamlit as st
 from io import BytesIO
 import streamlit.components.v1 as components
-from audiorecorder import audiorecorder], ignore_index=True
+from audiorecorder import audiorecorder, ignore_index=True
 
 
 
